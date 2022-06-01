fe4ed14e6d70dab906595701de15a02753c52957
rzhomba <theviper0235@gmail.com>
2022-06-01 10:54:52 +0300

    Temporarily remove not implemented features

--- src/components/Chat/Settings/SettingsSidebar.tsx
+++ src/components/Chat/Settings/SettingsSidebar.tsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import { selectSettings } from '../../../store/settings/settingsSlice'
 import { useAppSelector } from '../../../utils/hooks'
-import HelpOption from './Options/HelpOption'
 import CheatOption from './Options/CheatOption'
 import DecodingOption from './Options/DecodingOption'
-import UsersOption from './Options/UsersOption'
-import InviteOption from './Options/InviteOption'
-import KickOption from './Options/KickOption'
-import DeleteOption from './Options/DeleteOption'
 import LeaveOption from './Options/LeaveOption'
 import './SettingsSidebar.css'
 
@@ -20,19 +15,11 @@ const SettingsSidebar = () => {
         Settings and Info
       </div>
       <div className="Settings">
-        <HelpOption/>
         <CheatOption/>
         <DecodingOption/>
 
         <div className="Delimiter"></div>
 
-        <UsersOption/>
-        <InviteOption/>
-        <KickOption/>
-
-        <div className="Delimiter"></div>
-
-        <DeleteOption/>
         <LeaveOption/>
       </div>
     </div>
