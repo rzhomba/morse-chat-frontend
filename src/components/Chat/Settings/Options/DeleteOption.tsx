787f0768e9d111b39f222573b0ea30b5a8824e3f
rzhomba <theviper0235@gmail.com>
2022-06-14 18:52:45 +0300

    Delete room popup

--- src/components/Chat/Settings/Options/DeleteOption.tsx
+++ src/components/Chat/Settings/Options/DeleteOption.tsx
@@ -1,6 +1,23 @@
 import React from 'react'
 import SettingsOption from '../SettingsOption'
 import DeleteIcon from '#icons/delete.svg'
+import { displayPopup } from '../../../Utils/Popup'
+import './DeleteOption.css'
+
+const DeletePopup = () => {
+  const deleteRoom = () => {
+  }
+
+  return (
+    <div>
+      <div className="DeleteOptionLabel">Are you sure?</div>
+      <div className="DeleteOptionButtons">
+        <button className="Button ButtonSecondary" onClick={() => deleteRoom()}>Proceed</button>
+        <button className="Button">Cancel</button>
+      </div>
+    </div>
+  )
+}
 
 const DeleteOption = () => {
   return (
@@ -8,7 +25,10 @@ const DeleteOption = () => {
                     label="Delete room"
                     warning={true}
                     access={'admin'}
-                    onClick={() => {}}/>
+                    onClick={() => displayPopup({
+                      title: 'Delete Room',
+                      content: <DeletePopup/>
+                    })}/>
   )
 }
 
