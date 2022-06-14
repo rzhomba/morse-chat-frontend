5c96dbb121c92a7403372f210157a118d74a3585
rzhomba <theviper0235@gmail.com>
2022-06-14 14:50:37 +0300

    Invite user option

--- src/components/Chat/Settings/Options/InviteOption.tsx
+++ src/components/Chat/Settings/Options/InviteOption.tsx
@@ -1,12 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SettingsOption from '../SettingsOption'
 import UserAddIcon from '#icons/useradd.svg'
+import CopyIcon from '#icons/copy.svg'
+import { displayPopup } from '../../../Utils/Popup'
+import './InviteOption.css'
+
+const InvitePopup = () => {
+  const [copied, setCopied] = useState(false)
+  const roomUrl = window.location.href
+
+  const copy = async () => {
+    if (window.isSecureContext) {
+      await navigator.clipboard.writeText(roomUrl)
+    } else {
+      console.warn('Page is not served from a secure origin')
+    }
+
+    setCopied(true)
+    setTimeout(() => setCopied(false), 1500)
+  }
+
+  return (
+    <div>
+      <div className="InviteUserOptionLabel">Send to user the room url:</div>
+      <div className="InviteUserOptionField">
+        <div className="InviteUserOptionUrl">
+          {roomUrl}
+        </div>
+        <button className="InviteUserOptionCopy" onClick={() => copy()}>
+          <CopyIcon/>
+        </button>
+      </div>
+      <div className={`InviteUserOptionMessage ${copied ? 'InviteUserOptionCopied' : ''}`}>
+        Link copied to clipboard
+      </div>
+    </div>
+  )
+}
 
 const InviteOption = () => {
   return (
     <SettingsOption icon={UserAddIcon}
                     label="Invite user"
-                    onClick={() => {}}/>
+                    onClick={() => {
+                      displayPopup({
+                        title: 'Invite User',
+                        content: <InvitePopup/>
+                      })
+                    }}/>
   )
 }
 
