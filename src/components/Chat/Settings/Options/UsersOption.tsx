7504582c9c40d0b78504cc09b405fa55c9da363b
rzhomba <theviper0235@gmail.com>
2022-06-14 14:50:20 +0300

    Users list option refactor

--- src/components/Chat/Settings/Options/UsersOption.tsx
+++ src/components/Chat/Settings/Options/UsersOption.tsx
@@ -4,50 +4,57 @@ import { useAppSelector } from '../../../../utils/hooks'
 import SettingsOption from '../SettingsOption'
 import UserListIcon from '#icons/userlist.svg'
 import { displayPopup } from '../../../Utils/Popup'
+import './UsersOption.css'
 
-const UsersOption = () => {
+const UsersPopup = () => {
   const { chatUsers } = useAppSelector(selectChat)
 
+  return (
+    <table>
+      <thead>
+      <tr>
+        <th>Name</th>
+        <th>Joined</th>
+        <th>Role</th>
+        <th>Status</th>
+      </tr>
+      </thead>
+      <tbody>
+      {chatUsers.map(user => (
+        <tr key={user.name}>
+          <td>{user.name}</td>
+          <td className="UsersOptionDate">
+            {
+              new Date()
+                .toLocaleDateString('en-US', {
+                  month: 'short',
+                  day: 'numeric'
+                }) + ' ' +
+              new Date()
+                .toLocaleTimeString('en-US', {
+                  hour: '2-digit',
+                  minute: '2-digit',
+                  hour12: false
+                })
+            }
+          </td>
+          <td className="UsersOptionRole">{user.role.toUpperCase()}</td>
+          <td className="UsersOptionStatus">ONLINE</td>
+        </tr>
+      ))}
+      </tbody>
+    </table>
+  )
+}
+
+const UsersOption = () => {
   return (
     <SettingsOption icon={UserListIcon}
                     label="View users"
                     onClick={() => {
                       displayPopup({
                         title: 'Chat Users',
-                        content: <table>
-                          <thead>
-                          <tr>
-                            <th>Name</th>
-                            <th>Joined</th>
-                            <th>Role</th>
-                            <th>Status</th>
-                          </tr>
-                          </thead>
-                          <tbody>
-                          {chatUsers.map(user => (
-                            <tr key={user.name}>
-                              <td>{user.name}</td>
-                              <td align="center">
-                                {
-                                  new Date()
-                                    .toLocaleDateString('en-US', {
-                                      month: 'short',
-                                      day: 'numeric'
-                                    }) + ' ' +
-                                  new Date()
-                                    .toLocaleTimeString('en-US', {
-                                      hour: '2-digit',
-                                      minute: '2-digit',
-                                      hour12: false
-                                    })
-                                }
-                              </td>
-                              <td>{user.role.toUpperCase()}</td>
-                              <td>ONLINE</td>
-                            </tr>
-                          ))}
-                          </tbody>
-                        </table>
+                        content: <UsersPopup/>
                       })
                     }}/>
   )
